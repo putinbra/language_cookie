2eef923ee49ebbb7adfec90845638035e2f98e64
ʕ•́ᴥ•̀ʔっPutin <76955327+putinbra@users.noreply.github.com>
2021-04-20 18:24:46 +0200

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -1,41 +1,57 @@
 <?php
-session_start();
+
+if(isset($_POST['cookie'])) {
+    setcookie('allowed',true,strtotime("365 days"));
+}
 
 $acceptedLanguages  = ['de', 'en'];
 $language = 'en';
 
-if (isset($_GET['lang'])) {
-    if (isAcceptedLanguage($_GET['lang'])) {
-        $language = $_GET['lang'];
-        setLanguageCookie($language);
-    }
-} elseif (isset($_COOKIE['language'])) {
-    $language = $_COOKIE['language'];
-} elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && !empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-    $languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-
-    foreach ($languages as $tempLanguage) {
-        if (isAcceptedLanguage($tempLanguage)) {
-            $language = $tempLanguage;
+    if (isset($_GET['lang'])) {
+        if (isAcceptedLanguage($_GET['lang'])) {
+            $language = $_GET['lang'];
             setLanguageCookie($language);
-            break;
+        }
+    } elseif (isset($_COOKIE['language'])) {
+        $language = $_COOKIE['language'];
+    } elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && !empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+        $languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+        foreach ($languages as $tempLanguage) {
+            if (isAcceptedLanguage($tempLanguage)) {
+                $language = $tempLanguage;
+                setLanguageCookie($language);
+                break;
+            }
         }
     }
-}
 
-$path = $language . '/index.php';
+    $path = $language . '/index.php';
 
-if (file_exists($path)) {
-    require_once $path;
-} else {
-    require_once 'errors/error-lang-404.php';
-}
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        require_once 'C:xampp\htdocs\Website/errors/error-lang-404.php';
+    }
 
-function isAcceptedLanguage(string $language): bool {
-    static $acceptedLanguages = ['de', 'en'];
-    return in_array($language, $acceptedLanguages);
-}
+    function isAcceptedLanguage(string $language): bool
+    {
+        static $acceptedLanguages = ['de', 'en'];
+        return in_array($language, $acceptedLanguages);
+    }
 
-function setLanguageCookie(string $language): void {
-    setcookie('language', $language, strtotime('+90 days'));
+    function setLanguageCookie(string $language): void
+    {
+        setCookieIfAllowed('language', $language, strtotime('+90 days'));
+    }
+
+function setCookieIfAllowed(string $name,string $value = "", int $expires = 0 , string $path = "" , string $domain = "" , bool $secure = false , bool $httponly = false ):bool {
+
+    $cookiesAllowed = isset($_COOKIE['allowed']);
+    if(false===$cookiesAllowed){
+        return false;
+    }
+
+    return setcookie($name,$value,$expires,$path,$domain,$secure,$httponly);
+
 }
